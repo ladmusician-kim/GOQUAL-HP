79216d76bf976598eba275a557437b42acc58bb6
김동진 <ladmusician.kim@gmail.com>
2015-08-10 14:17:05 +0900

    core detail 디자인 수정

--- application/views/_CORE/detail.php
+++ application/views/_CORE/detail.php
@@ -1,14 +1,14 @@
 <section class="gq-core">
-    <div class="container">
+    <div class="container gq-core-container">
         <div class="row text-center">
             <div class="col-lg-12 gq-core-profile text-center">
                 <img class="gq-core-profile-img" src="<?php echo base_url() ?>static/img/member/core_profile.png">
             </div>
             <div class="col-lg-12 gq-core-user">
-                <?php echo $item->username ?>
+                <?php echo $item->title ?>
             </div>
             <div class="col-lg-12 gq-core-created">
-                <?php echo date ("Y-m-d",strtotime($item->updated)); ?>
+                <?php echo date ("Y-m-d",strtotime($item->updated)); ?> | <?php echo $item->username ?>
             </div>
             <div class="col-lg-12 gq-core-content">
                 <?php echo $item->content ?>
