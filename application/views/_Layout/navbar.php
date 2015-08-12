e67683d89614e11a39442ad54bb0a893285dae5b
김동진 <ladmusician.kim@gmail.com>
2015-08-12 13:26:34 +0900

    navbar 디자인 수정

--- application/views/_Layout/navbar.php
+++ application/views/_Layout/navbar.php
@@ -1,4 +1,4 @@
-<nav class="navbar navbar-default navbar-static-top gq-navbar 'gq-main-navbar">
+<nav class="navbar navbar-default navbar-static-top gq-navbar gq-main-navbar">
     <div class="container">
         <div class="navbar-header">
             <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar"
@@ -8,7 +8,9 @@
                 <span class="icon-bar"></span>
                 <span class="icon-bar"></span>
             </button>
-            <!--<a class="navbar-brand gq-main-menu-brand" href="#">GOQUAL</a>-->
+            <a class="navbar-brand" href="#">
+                <img src="<?= site_url('/static/img/home/gq_ci.png')?>" >
+            </a>
         </div>
         <div id="navbar" class="navbar-collapse collapse gq-navbar-collapse" aria-expanded="false" style="height: 1px;">
             <ul class="nav navbar-nav navbar-right gq-main-menu-nav">
@@ -23,3 +25,37 @@
         </div>
     </div>
 </nav>
+<!--
+<nav class="navbar navbar-default navbar-fixed-top gq-navbar gq-main-navbar">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar" aria-expanded="false" aria-controls="navbar">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" href="#">Project name</a>
+        </div>
+        <div id="navbar" class="collapse navbar-collapse gq-navbar-collapse pull-right">
+            <ul class="nav navbar-nav gq-main-menu-nav">
+                <li class="active"><a href="#">Home</a></li>
+                <li><a href="#about">About</a></li>
+                <li><a href="#contact">Contact</a></li>
+                <li class="dropdown">
+                    <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span class="caret"></span></a>
+                    <ul class="dropdown-menu">
+                        <li><a href="#">Action</a></li>
+                        <li><a href="#">Another action</a></li>
+                        <li><a href="#">Something else here</a></li>
+                        <li role="separator" class="divider"></li>
+                        <li class="dropdown-header">Nav header</li>
+                        <li><a href="#">Separated link</a></li>
+                        <li><a href="#">One more separated link</a></li>
+                    </ul>
+                </li>
+            </ul>
+        </div>
+    </div>
+</nav>
+-->
